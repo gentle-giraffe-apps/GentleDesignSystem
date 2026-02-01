4f422c38376a682fe1833a69119c365d9f3ac836
gentle-giraffe-apps <gentlegiraffeapps@gmail.com>
2026-01-31 23:21:32 -0800

    feat: add snapshot tests

--- Package.swift
+++ Package.swift
@@ -16,6 +16,9 @@ let package = Package(
             targets: ["GentleDesignSystem"]
         ),
     ],
+    dependencies: [
+        .package(url: "https://github.com/pointfreeco/swift-snapshot-testing", from: "1.17.0"),
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
@@ -25,7 +28,10 @@ let package = Package(
         ),
         .testTarget(
             name: "GentleDesignSystemTests",
-            dependencies: ["GentleDesignSystem"]
+            dependencies: [
+                "GentleDesignSystem",
+                .product(name: "SnapshotTesting", package: "swift-snapshot-testing"),
+            ]
         ),
     ]
 )
